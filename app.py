a122ba3c64c229e5d731c23fb8e9d5eee643b116
mezcurra-hash <mezcurra@cemic.edu.ar>
2026-01-08 13:32:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ try:
         with col_graf2:
             st.subheader("Cantidad de turnos (Ts y AS")
             datos_canales = {
-                'Plataforma': ['Ts (Tel)', 'AS (Tel)', 'Total (Tel)'],
+                'Canal': ['Ts (Tel)', 'AS (Tel)', 'Total (Tel)'],
                 'Turnos': [datos_mes['TURNOS_CONS_TEL'], datos_mes['TURNOS_PRACT_TEL'], datos_mes['TURNOS_TOTAL_TEL']]
             }
             fig_bar = px.bar(datos_canales, x='Canal', y='Turnos', color='Canal')
